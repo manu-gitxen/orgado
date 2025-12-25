459c026a1a85e6ddb8c6b2b488193bb2309d75ce
manu <tonbitx@proton.me>
2025-12-25 12:30:03 +0530

    fixed Header design

--- orgado-web/src/components/Header.jsx
+++ orgado-web/src/components/Header.jsx
@@ -1,26 +1,163 @@
-import React from 'react'
+import React from "react";
+import homeLogo from "../assets/logo/logo2.png";
+import "../assets/Styles/Header.css";
+import { FiChevronDown } from "react-icons/fi";
+
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container">
-        <a className="navbar-brand fw-bold" href="#"><img src="" alt="" /></a>
-        
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+    <nav className="navbar navbar-expand-lg  py-3">
+      <div className="container-fluid px-2">
+        {/* Logo */}
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img src={homeLogo} alt="Orgado" height="40" className="me-2" />
+        </a>
+
+        {/* Toggle */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNav"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">About</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Shop</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+
+     
+        <div className="collapse navbar-collapse" id="mainNav">
+  
+          <ul className="navbar-nav  gap-5">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link  dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+              >
+                Home <FiChevronDown className="dropdown-icon" />
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Home 1
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Home 2
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Home 3
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link  dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+              >
+                Shop <FiChevronDown className="dropdown-icon" />
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Shop Grid
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Shop List
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Product Details
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link  dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+              >
+                Pages <FiChevronDown className="dropdown-icon" />
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Cart
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Checkout
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link  dropdown-toggle" href="#">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link  dropdown-toggle" href="#">
+                Contact
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link  dropdown-toggle" href="#">
+                Track Order
+              </a>
+            </li>
           </ul>
+
+          {/* Right Side */}
+          <div className="d-flex align-items-center gap-3">
+            {/* Search */}
+            <div className="search-box position-relative">
+              <input
+                type="text"
+                className=""
+                placeholder="Search products..."
+              />
+              <i className="bi bi-search search-icon"></i>
+            </div>
+
+            {/* Cart */}
+            <div className="icon-box position-relative">
+              <i className="bi bi-bag"></i>
+              <span className="badge">0</span>
+            </div>
+
+            {/* Wishlist */}
+            <div className="icon-box position-relative">
+              <i className="bi bi-heart"></i>
+              <span className="badge">0</span>
+            </div>
+
+            {/* User */}
+            <div className="icon-box">
+              <i className="bi bi-person"></i>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- orgado-web/src/components/TopBar.jsx
+++ orgado-web/src/components/TopBar.jsx
@@ -1,37 +1,45 @@
 import React from 'react'
-import { FaMapMarkerAlt,FaFacebookF,FaLinkedinIn,FaInstagram,FaTwitter,   } from "react-icons/fa";
+import { FaMapMarkerAlt,FaFacebookF,FaLinkedinIn,FaInstagram,FaTwitter, FaPinterest } from "react-icons/fa";
 import { FiPhone } from "react-icons/fi";
 import '../assets/Styles/TopBar.css'
+
 const TopBar = () => {
   return (
     <div className="top-box ">
       <div className="container-fluid d-flex justify-content-between top-bar-1"> 
       
-        <div className="contact-info">
-            <span ><FiPhone /> +(02) 587 - 898 -250</span>
-          <span className="ms-3"><FaMapMarkerAlt /> Favicon, New York, USA - 254230</span>
+        <div className="contact-info d-flex">
+            <a href='tel:+02587898250'><span className='' ><FiPhone className="phone-left" /> +(02) 587 - 898 -250</span></a>
+            <div className="v-bar-1">|</div>
+          <span className=""><FaMapMarkerAlt className='mx-1' /> Favicon, New York, USA - 254230</span>
           
         </div>
 
     
-        <div className="social-icons">
-          <span className="me-2"><FaFacebookF /></span>
-          <span className="me-2"><FaTwitter /></span>
-          <span className="me-2"><FaInstagram /></span>
-          <span><FaLinkedinIn /></span>
+        <div className="social-icons d-flex">
+          <span className=""><FaFacebookF /></span>
+          <div className="v-bar-1">|</div>
+          <span className=""><FaTwitter /></span>
+          <div className="v-bar-1">|</div>
+          <span className=""><FaInstagram /></span>
+          <div className="v-bar-1">|</div>
+          <span><FaPinterest/></span>
         </div>
       </div>
       <div className="container-fluid top-bar-2"> 
       
         <div className="preference-info d-flex justify-content-between  align-items-center">
-          <div className='d-flex gap-3 mt-1'>
-            <h6>About</h6>
-            <h6>My account</h6>
+          <div className='d-flex gap-3 mt-1 top-bar-2-links'>
+            <a href="#"><h6>About Us</h6></a>
+            <div className="v-bar-2">|</div>
+            <a href="#"><h6>My account</h6></a>
+            
             </div>
             <div className='d-flex gap-3 top-bar-selects'>
               <select name="" id="">
                 <option value="">English</option>
               </select>
+              <div className="v-bar-2">|</div>
               <select name="" id="">
                 <option value="">USD</option>
               </select>
